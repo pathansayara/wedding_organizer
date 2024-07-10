d6e2a7ed5a5306e1584bba80f90d750904bb6924
Sayara Pathan <134247755+pathansayara@users.noreply.github.com>
2024-07-10 12:03:05 +0530

    wedding organizer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './menu/home/home.component';
+import { AboutComponent } from './menu/about/about.component';
+import { ServicesComponent } from './menu/services/services.component';
+import { TestimonialComponent } from './menu/testimonial/testimonial.component';
+import { ContactComponent } from './menu/contact/contact.component';
+import { GallaryComponent } from './menu/gallary/gallary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'gallary',
+    component:GallaryComponent
+  },
+  {
+    path:'services',
+    component:ServicesComponent
+  },
+  {
+    path:'testimonial',
+    component:TestimonialComponent
+  },
+  {
+    path:'contact',
+    component:ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './menu/home/home.component';
+import { AboutComponent } from './menu/about/about.component';
+import { ServicesComponent } from './menu/services/services.component';
+import { TestimonialComponent } from './menu/testimonial/testimonial.component';
+import { ContactComponent } from './menu/contact/contact.component';
+import { GallaryComponent } from './menu/gallary/gallary.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    TestimonialComponent,
+    ContactComponent,
+    GallaryComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
